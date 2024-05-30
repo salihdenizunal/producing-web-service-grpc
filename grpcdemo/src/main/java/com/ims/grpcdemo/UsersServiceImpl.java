69f3dd917bd3c5797de6b950e44e6141e40b6187
Mehmet Yıldız <yildiz.mehmet@metu.edu.tr>
2024-05-30 18:01:54 +0300

    feat: Update capabilities for different user types
    This commit updates the capabilities for different user types in the UsersServiceImpl class. The ADMIN user can now create, update, delete, and read both users and products. The CUSTOMER user can read products and create, update, delete, and read their orders. The IMPORT_MANAGER user can create, update, delete, and read products and orders. The SUPPLIER user can create, update, delete, and read products, as well as regulate the stock. Additionally, a default message is set for invalid user roles.

--- grpcdemo/src/main/java/com/ims/grpcdemo/UsersServiceImpl.java
+++ grpcdemo/src/main/java/com/ims/grpcdemo/UsersServiceImpl.java
@@ -9,23 +9,29 @@ public class UsersServiceImpl extends UsersServiceGrpc.UsersServiceImplBase {
         CapabilityOfUserResponse response;
         switch (req.getUserType()) {
             case ADMIN:
-                response = CapabilityOfUserResponse.newBuilder().setCapabilities("ADMIN can do everything they want.")
+                response = CapabilityOfUserResponse.newBuilder().setCapabilities(
+                        "ADMIN can create, update, delete, read users. Also, they can create, update, delete, read products. They can do everything they want.")
                         .build();
                 break;
             case CUSTOMER:
-                response = CapabilityOfUserResponse.newBuilder().setCapabilities("CUSTOMER can do everything they want.")
+                response = CapabilityOfUserResponse.newBuilder()
+                        .setCapabilities("CUSTOMER can read products and create, update, delete, read their orders.")
                         .build();
                 break;
             case IMPORT_MANAGER:
-                response = CapabilityOfUserResponse.newBuilder().setCapabilities("IMPORT_MANAGER can do everything they want.")
+                response = CapabilityOfUserResponse.newBuilder()
+                        .setCapabilities(
+                                "IMPORT MANAGER can create, update, delete, read products. Also, they can create, update, delete, read orders.")
                         .build();
                 break;
             case SUPPLIER:
-                response = CapabilityOfUserResponse.newBuilder().setCapabilities("SUPPLIER can do everything they want.")
+                response = CapabilityOfUserResponse.newBuilder()
+                        .setCapabilities(
+                                "SUPPLIER can create, update, delete, read products. Also, they can regulate the stock.")
                         .build();
                 break;
             default:
-                response = CapabilityOfUserResponse.newBuilder().setCapabilities("Not valid user type.").build();
+                response = CapabilityOfUserResponse.newBuilder().setCapabilities("Not valid user role.").build();
                 break;
         }
         responseObserver.onNext(response);
